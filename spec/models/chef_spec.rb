16abf44bfc069ca273e5dde748e2be76e05d6f06
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-04-17 16:09:42 -0600

    Final Commit

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Chef, type: :model do
   end
   describe "relationships" do
     it {should have_many :dishes}
+    it {should have_many(:dish_ingredients).through(:dishes)}
+    it {should have_many(:ingredients).through(:dish_ingredients)}
   end
 
   
